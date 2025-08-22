a592ee9c10bf33cdf17fb684affc2964e2dd9100
adishreyash <104453585+adishreyash@users.noreply.github.com>
2025-08-22 12:39:36 +0530

    Update Assignment1Task1.py

--- Assignment1Task1.py
+++ Assignment1Task1.py
@@ -1,6 +1,5 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
-#Addition
 print("Addition: ",num1 + num2)
 print("Subtraction: ",num1 - num2)
 print("Multiplication: ",num1 * num2)
